a375352bc8cbac539fca7abf8c6e5a29c8a8ff7e
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2023-02-11 11:33:34 +0100

    feat(Builder): Add ConstraintViolationListCollectionBuilder to simplify the validation processs

---
+++ src/ConstraintViolationListCollectionBuilder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Atournayre\EntityValidation;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Validation;
+
+class ConstraintViolationListCollectionBuilder
+{
+    private array $inputs;
+    private array $constraints;
+
+    /**
+     * This class cannot be instantiated.
+     */
+    private function __construct()
+    {
+    }
+
+    public static function create(): ConstraintViolationListCollectionBuilder
+    {
+        return new self();
+    }
+
+    public function add($input, $constraint): static
+    {
+        $key = md5($input.microtime());
+        $this->inputs[$key] = $input;
+        $this->constraints[$key] = $constraint;
+
+        return $this;
+    }
+
+    public function build(): ConstraintViolationListCollection
+    {
+        if ($this->constraints === []) {
+            return self::constraintViolationListCollection(new ConstraintViolationList());
+        }
+
+        $constraints = new Assert\Collection($this->constraints);
+
+        $constraintViolationList = Validation::createValidator()
+            ->validate($this->inputs, $constraints);
+
+        return self::constraintViolationListCollection($constraintViolationList);
+    }
+
+    private static function constraintViolationListCollection(iterable $elements): ConstraintViolationListCollection
+    {
+        return new ConstraintViolationListCollection($elements);
+    }
+}
